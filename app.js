a83332723014e83149a7a636abf81328cfa181a6
Aniket Mote <moteaniket952@gmail.com>
2023-12-10 19:32:51 +0530

    Ep-01 basics of react

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "HII i am aniket"),
+    React.createElement("h1", {}, "HII i am mote"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "HII i am nitin"),
+    React.createElement("h1", {}, "HII i am mote"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
