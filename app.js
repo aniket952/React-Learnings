a3a145af8ac88e59c95c9504544f3c5a01de92d1
Aniket Mote <moteaniket952@gmail.com>
2024-05-16 21:41:01 +0530

    Ep-01

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const myName = React.createElement("h1",{}, "my name is aniket and i am working in accenture and i am greatful of that");
+      const treeRoot = ReactDOM.createRoot(document.getElementById("rootDiv"));
+      treeRoot.render(myName);
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "HII i am aniket"),
