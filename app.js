04b7ab616e55dc5d97c1d82bc0108e44e11f555d
Aniket Mote <moteaniket952@gmail.com>
2024-05-17 19:33:14 +0530

    ep-02

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const myName = React.createElement("h1",{}, "my name is aniket and i am working in accenture and i am greatful of that");
-      const treeRoot = ReactDOM.createRoot(document.getElementById("rootDiv"));
-      treeRoot.render(myName);
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -11,9 +8,9 @@ const heading = React.createElement("div", { id: "parent" }, [
     React.createElement("h1", {}, "HII i am mote"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "HII i am nitin"),
+    React.createElement("h1", {}, "HII i am nitin"), 
     React.createElement("h1", {}, "HII i am mote"),
   ]),
 ]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("rootDiv"));
 root.render(heading);
