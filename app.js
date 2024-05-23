81332fa64430135e10181a9ac0c501feb70d80f6
Aniket Mote <moteaniket952@gmail.com>
2024-05-23 18:06:27 +0530

    ep3

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//without using JSX, core way to do it
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "HII i am aniket"),
+//     React.createElement("h1", {}, "HII i am mote"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "HII i am nitin"),
+//     React.createElement("h1", {}, "HII i am mote"),
+//   ]),
+// ]);
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "HII i am aniket"),
-    React.createElement("h1", {}, "HII i am mote"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "HII i am nitin"), 
-    React.createElement("h1", {}, "HII i am mote"),
-  ]),
-]);
+//with useing JSX
+const heading = (
+  <div className="aniket" id="aniket">
+    <h1>my name is aniket mote i am wotking in accenture</h1>
+    <h2>my name is aniket</h2>
+  </div>
+);
+
+//functional Component
+const ChildComponenet = () => (
+  <div className="dp">
+    <h1>dp</h1>
+    <h2>mote</h2>
+  </div>
+);
+const HeadingComponent = () => (
+  <div className="aniketmote">
+    <h1>aniket</h1>
+    <h2>mote</h2>
+    <ChildComponenet />
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("rootDiv"));
-root.render(heading);
+// root.render(heading);
+root.render(<HeadingComponent />);
